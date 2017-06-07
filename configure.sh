7f46273284a9545ff923c8ca4006a0658da3a842
VHarisop <the.machine.headed@gmail.com>
2017-06-07 17:53:33 +0200

    Removed -j argument from make

--- configure.sh
+++ configure.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# Number of cores to use for make
-CORES=${1:-1}
 CALLDIR=$(pwd)
 
 install_paramcoq() {
 	echo -n "Installing paramcoq..."
-	cd paramcoq && make -j${CORES} && make install && \
+	cd paramcoq && make && make install && \
 		echo "Installation complete!"
 	if [ $? -eq 0 ]; then
 		cd ${CALLDIR} && return 0
@@ -18,7 +16,7 @@ install_paramcoq() {
 install_coqeal_theory() {
 	echo "Installing CoqEAL.theory..."
 	coq_makefile -f _CoqProject_theory -o Makefile.theory
-	make -j${CORES} -f Makefile.theory && make install -f Makefile.theory && \
+	make -f Makefile.theory && make install -f Makefile.theory && \
 		echo "Installation complete!"
 	return $?
 }
@@ -26,7 +24,7 @@ install_coqeal_theory() {
 install_coqeal_refinements() {
 	echo "Installing CoqEAL.refinements..."
 	coq_makefile -f _CoqProject_refine -o Makefile.refine
-	make -j${CORES} -f Makefile.refine && make install -f Makefile.refine && \
+	make -f Makefile.refine && make install -f Makefile.refine && \
 		echo "Installation complete!"
 	return $?
 }

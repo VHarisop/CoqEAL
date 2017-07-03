10f12b1bfaaae678b6e4ef4e9a850cf202f7956b
VHarisop <the.machine.headed@gmail.com>
2017-07-03 18:23:46 +0200

    Some progress on ordseq.v

--- refinements/ordseq.v
+++ refinements/ordseq.v
@@ -15,26 +15,20 @@ Local Open Scope computable_scope.
 
 Section ordseq_op.
 
-Variable A : eqType.
+(* Variable A : eqType. *)
 Variable N : Type.
 
-Definition ordseq {A} := seq A.
+Definition ordseq {m} := seq 'I_m.
 
-Context `{zero_of A, one_of A, cast_of A}.
-Context `{add_of A, sub_of A, eq_of A, leq_of A, lt_of A}.
-(* Note: A is supposed to be a refinement of nat *)
-Context `{Hnat: spec_of A nat}.
-Context `{zero_of N, one_of N, add_of N, eq_of N, spec_of N nat}.
+Context `{zero_of N, one_of N, add_of N, eq_of N, leq_of N, lt_of N, spec_of N nat}.
 
-Global Instance cast_ordseq : cast_of A ordseq := fun x => [:: x].
-Global Instance ordseq0 : zero_of (@ordseq A) := [::].
-Global Instance ordseq_singleton x : one_of (@ordseq A) := [:: x].
-Global Instance eq_ordseq : eq_of ordseq := @eqseq A.
+Global Instance ordseq0 {m} : zero_of (@ordseq m) := [::].
+Global Instance eq_ordseq {m} : eq_of (@ordseq m) := @eqseq (ordinal_eqType m).
 
-Fixpoint ordseq_sublist (p q : ordseq) : bool :=
+Fixpoint ordseq_sublist {m} (p q : ordseq) : bool :=
   (* mem_aux : returns true/false, plus the remaining list after the match
                in case the element has indeed been found *)
-  let fix mem_aux (x : A) (p' : ordseq) : bool * ordseq := match p' with
+  let fix mem_aux (x : 'I_m) (p' : ordseq) : bool * ordseq := match p' with
   | [::] => (false, [::])
   | h :: t =>
       (* If element is found, return true and the rest of the other set *)
@@ -51,62 +45,49 @@ Fixpoint ordseq_sublist (p q : ordseq) : bool :=
   end.
 
 (* Difference of two ordseqs *)
-Definition ordseq_sub : sub_of (@ordseq A) :=
+Definition ordseq_sub {m} : sub_of (@ordseq m) :=
   let fix aux (a : ordseq) : ordseq -> ordseq := match a with
   | [::] => fun _ => [::]
   | h :: t => (fix aux_comp (b : ordseq) : ordseq :=
       match b with
       | [::] => a
       | h' :: t' =>
-        if (h < h') then h :: (aux t b)
+        if (h < h')%N then h :: (aux t b)
         else if (h == h') then aux t t'
         else aux_comp t'
       end)
   end
   in aux.
 
+Definition ordcmp {m} := fun (x y : 'I_m) => ((nat_of_ord x) <= (nat_of_ord y))%N.
+
 (* Union of two ordseqs *)
-Definition ordseq_add : add_of (@ordseq A) :=
-  fun a b => merge (fun x y => x <= y) a (ordseq_sub a b).
+Definition ordseq_add {m} `{spec_of N 'I_m} : add_of (@ordseq m) :=
+  fun a b => merge ordcmp a (ordseq_sub a b).
 
 (** Returning if an element is a member of a set *)
-Fixpoint ordseq_mem (x : A) (p : ordseq) : bool := match p with
+Fixpoint ordseq_mem {m} (x : 'I_m) (p : ordseq) : bool := match p with
   | [::] => false
-  | h :: t => (eq_op h x) || ordseq_mem x t
+  | h :: t => (h == x) || ordseq_mem x t
   end.
 
-(* Make use of spec_of A nat, which is in our context, to map an
-   @ordseq A to a seq nat *)
-Definition map_spec s := map (@spec A nat _) s.
+Global Instance implem_ordseq m : implem_of {set 'I_m} ordseq :=
+  fun s => [seq i | i <- enum s].
 
-(* Specification of an ordseq *)
-Global Instance spec_ordseq : spec_of (@ordseq A) (seq nat) :=
-  fun s => map_spec s.
+Global Instance spec_ordseq {m} `{spec_of N 'I_m} :
+  spec_of (@ordseq m) {set 'I_m} := fun s =>
+  [set i | i \in s].
 
 (** Size of a set *)
-Global Instance size_seqset : size_of (@seqset A) N :=
+Global Instance size_seqset {m} : size_of (@ordseq m) N :=
   let fix aux cnt p := match p with
   | [::] => cnt
   | _ :: t => aux (cnt + 1) t
   end
   in aux 0.
 
-(** Adding an element to a set *)
-Definition add_elem sset elem := if seqset_mem elem sset then sset else elem :: sset.
-
-(** Adding two seqsets *)
-Global Instance union_seqset : add_of seqset :=
-  let fix aux p elems := match p with
-  | [::] => elems
-  | h :: t => aux t (add_elem elems h)
-  end
-  in aux.
-
-End seqset_op.
-
-Parametricity cast_seqset.
-Parametricity eq_seqset.
-Parametricity seqset0.
-Parametricity seqsingleton.
-Parametricity size_seqset.
-Parametricity union_seqset.
+(* In the spirit of Rseqmx...*)
+CoInductive Rordseq {m1 m2} (rm : nat_R m1 m2) :
+  {set 'I_m1} -> @ordseq m2 -> Type := Rordseq_spec (I : {set 'I_m1}) (J : ordseq) of
+  (forall j, j \in J -> ((nat_of_ord j) < m1)%N) &
+  (uniq J) & (sorted ordcmp J) : Rordseq rm I J.

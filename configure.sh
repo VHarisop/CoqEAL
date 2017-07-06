9bf2a0d6f191038905d473b907458199a3a1d906
VHarisop <the.machine.headed@gmail.com>
2017-07-06 11:34:13 +0200

    Split installation/configuration files

--- configure.sh
+++ configure.sh
@@ -2,31 +2,16 @@
 
 CALLDIR=$(pwd)
 
-install_paramcoq() {
-	echo -n "Installing paramcoq..."
-	cd paramcoq && make && make install && \
-		echo "Installation complete!"
-	if [ $? -eq 0 ]; then
-		cd ${CALLDIR} && return 0
-	else
-		cd ${CALLDIR} && return 1
-	fi
-}
-
-install_coqeal_theory() {
-	echo "Installing CoqEAL.theory..."
+configure_coqeal_theory() {
+	echo "Building Makefile for CoqEAL.theory..."
 	coq_makefile -f _CoqProject_theory -o Makefile.theory
-	make -f Makefile.theory && make install -f Makefile.theory && \
-		echo "Installation complete!"
 	return $?
 }
 
-install_coqeal_refinements() {
-	echo "Installing CoqEAL.refinements..."
+configure_coqeal_refinements() {
+	echo "Building Makefile for CoqEAL.refinements..."
 	coq_makefile -f _CoqProject_refine -o Makefile.refine
-	make -f Makefile.refine && make install -f Makefile.refine && \
-		echo "Installation complete!"
 	return $?
 }
 
-install_paramcoq && install_coqeal_theory && install_coqeal_refinements
+configure_coqeal_theory && configure_coqeal_refinements

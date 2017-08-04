df294203f63b507990d8220df257c7bef23ec12c
VHarisop <the.machine.headed@gmail.com>
2017-08-04 16:00:01 +0200

    Introduced separate section for option-only refinements

--- refinements/pick.v
+++ refinements/pick.v
@@ -3,18 +3,15 @@ From mathcomp Require Import choice fintype bigop matrix.
 
 From CoqEAL Require Import hrel param refinements seqmx seqmx_complements.
 
-Section pick_refinement.
+Section option_refinements.
 
-Instance eq_option {m} :
-  forall (x y : option 'I_m), option_R eq x y -> refines eq x y.
-Proof.
-  rewrite refinesE => x y /=; case; last by [].
-  by move => a b ->.
-Qed.
+Global Instance eq_option {T : Type} :
+  forall (x y : option T), option_R eq x y -> refines eq x y.
+Proof. by rewrite refinesE => x y /=; case => // ? ? ->. Qed.
 
-Global Instance Roption_eq m :
+Global Instance Roption_eq {T : eqType} :
   refines (option_R eq ==> option_R eq ==> bool_R)
-          (@eqtype.eq_op (option_eqType (ordinal_finType m))) eqtype.eq_op.
+          (@eqtype.eq_op (option_eqType T)) eqtype.eq_op.
 Proof.
   rewrite refinesE=> x x' /= hx y y' /= hy.
   have -> : eq x x' by apply refinesP; apply: eq_option; exact: hx.
@@ -22,13 +19,27 @@ Proof.
   by case/boolP: (x' == y').
 Qed.
 
-Global Instance Roption_ord_eq m :
+Global Instance refines_none {T1 T2 : Type} {R : T1 -> T2 -> Type} :
+  refines (option_R R) None None.
+Proof. by rewrite refinesE; constructor. Qed.
+
+Global Instance refines_some {T1 T2 : Type} {R : T1 -> T2 -> Type} :
+  refines (R ==> option_R R) Some Some.
+Proof. rewrite !refinesE /Rord => x y /= Hxy. by constructor. Qed.
+
+End option_refinements.
+
+(** A refinement for the pick operator, focused on its use within
+    the Gaussian elimination routine. *)
+Section pick_refinement.
+
+Global Instance Roption_Rord m :
   refines (
   option_R (Rord (nat_Rxx m)) ==> option_R (Rord (nat_Rxx m))  ==> bool_R)
           (@eqtype.eq_op (option_eqType (ordinal_finType m))) eqtype.eq_op.
 Proof.
   rewrite !refinesE /Rord => x y /= Hxy z w /= Hzw.
-  elim: Hxy; elim: Hzw => // a1 a2 /= <- b1 b2 /= <-.
+  elim: Hxy; elim: Hzw => // a1 _ /= <- b1 _ /= <-.
   rewrite /eqtype.eq_op /=. have <- : (b1 == a1) = (b1 == a1 :> nat) by [].
   by case: (b1 == a1).
 Qed.
@@ -51,6 +62,8 @@ Proof.
 Admitted.
 *)
 
+(** Can we somehow avoid writing a different instance for every
+    pred that we want to refine ? *)
 Instance refines_Rord_ltn {m : nat} (n : nat) :
   refines (Rord (nat_Rxx n) ==> eq)
   (fun x : 'I_n => x < m) (fun x : nat => x < m).
@@ -70,16 +83,6 @@ Instance refines_Rord_leqn {m : nat} (n : nat) :
   (fun x : 'I_n => x <= m) (fun x : nat => x <= m).
 Proof.
   by rewrite !refinesE /Rord => x y /= ->.
-Qed.
-
-Instance refines_none n :
-  refines (option_R (Rord (nat_Rxx n))) None None.
-Proof. by rewrite refinesE; constructor. Qed.
-
-Instance refines_some n :
-  refines (Rord (nat_Rxx n) ==> option_R (Rord (nat_Rxx n))) Some Some.
-Proof.
-  rewrite !refinesE /Rord => x y /= Hxy. by constructor.
 Qed.
 
 Goal (@pick (ordinal_finType 5) (fun i => i < 0)) == None.
